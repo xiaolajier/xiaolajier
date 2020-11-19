458eaff1239bce5e6a85729d88198f40e25bed02
xiaolajier <74294657+xiaolajier@users.noreply.github.com>
2020-11-19 19:49:43 +0800

    Create 111910.性别.c

---
+++ 111910.性别.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("性别查询器\n");
+	scanf("%d",&a);
+	if(a==1)
+	{
+	printf("性别是：");
+	printf("男\n");
+	}
+	else if(a==0)
+	{
+	printf("性别是：");
+	printf("女\n");
+	}
+}
