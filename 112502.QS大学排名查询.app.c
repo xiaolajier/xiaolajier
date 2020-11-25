8dbfb0d93e6c27600c02ac020ed73aa8c44276d1
xiaolajier <74294657+xiaolajier@users.noreply.github.com>
2020-11-25 09:52:34 +0800

    还是IF.ELSE IF,ELSE

---
+++ 112502.QS大学排名查询.app.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("QS全球大学排名\n");
+	scanf("%d",&a);
+	if(a=1)
+	{
+		printf("麻省理工学院");
+	}
+	else if(a=2)
+	{
+		printf("斯坦福大学");
+	}
+	else if(a=3)
+	{
+		printf("哈佛大学");
+	}
+	else if(a=4)
+	{
+		printf("牛津大学");
+	}
+	else if(a=5)
+	{
+		printf("加利福尼亚理工学院");
+	}
+	else if(a=6)
+	{
+		printf("ETH学院");
+	}
+	else if(a=7)
+	{
+		printf("剑桥大学");
+	}
+	else if(a=8)
+	{
+		printf("伦敦大学学院");
+	}
+	else if(a=9)
+	{
+		printf("帝国理工学院");
+	}
+	else if(a=10)
+	{
+		printf("芝加哥大学");
+	}
+	else
+	{
+		printf("老铁，输1-10的可以嘛");
+	}
+}
