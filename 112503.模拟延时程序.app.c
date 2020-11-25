154dcc430e5e96b0a803818db6b15c86bf357c19
xiaolajier <74294657+xiaolajier@users.noreply.github.com>
2020-11-25 17:18:22 +0800

    Create 112503.模拟延时程序.app.c

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{	
+	printf("昨天11点04\n");
+	printf("你们已成为好友，可以开始聊天啦\n");
+	printf("                           \n");
+	printf("00点37\n");
+	printf("我：嗨，现在才和妳说话不知是不是有点突然，妳可能也睡着了\n");
+	Sleep(3000);
+	printf("妳：咦，没有哦\n");
+	Sleep(2000);
+	printf("我：妳也是夜猫嘛(哭笑)\n");
+	Sleep(2000);
+}
