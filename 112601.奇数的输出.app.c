8868d2d60f1b7ddd939b55bb69775c170af4b649
xiaolajier <74294657+xiaolajier@users.noreply.github.com>
2020-11-26 19:58:56 +0800

    两种方法，while或for

---
+++ 112601.奇数的输出.app.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int a=1;
+	printf("[0,1024]内奇数的输出：\n");
+	while(a<=1024)
+	{
+		printf("%d\n",a);
+		a=a+2;
+	}
+
+}
+
+
+
+//#include<stdio.h>
+//void main()
+//{
+	//int a;
+	//printf("[0,1024]内奇数的输出：\n");
+	//for(a=1;a<=1024;a=a+2)
+	//{
+		//printf("%d\n",a);
+	//}
+
+//}
