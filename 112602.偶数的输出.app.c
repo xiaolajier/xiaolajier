4eae032781c0b55d3b6f80e6669aec5d4b6560c9
xiaolajier <74294657+xiaolajier@users.noreply.github.com>
2020-11-26 20:05:21 +0800

    两种方法，while或for

---
+++ 112602.偶数的输出.app.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{	
+	int a=0;
+	printf("[0,1024]内偶数的输出：\n");
+	while(a<=1024)
+	{
+		printf("%d\n",a);
+		a=a+2;
+	}
+}
+
+
+//#include<stdio.h>
+//void main()
+//{
+	//int a;
+	//printf("[0,1024]内偶数的输出：\n");
+	//for(a=0;a<=1024;a=a+2)
+	//{
+		//printf("%d\n",a);
+	//}
+
+//}
