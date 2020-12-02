43aed1500070c693e072af7ad5be480dfb381543
xiaolajier <74294657+xiaolajier@users.noreply.github.com>
2020-12-02 17:00:23 +0800

    未会，%的用法，s%,  60%

---
+++ 120203.秒转时分秒.app.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int h,m,s;
+	scanf("%d",&s);
+	if(s>=0 && s<=86399)
+	{
+	h=s/3600;
+	m=s/60%60;
+	s%=60;
+	printf("%d:%d:%d\n",h,m,s);
+	}
+	else
+	{
+		printf("已超过23:59:59，请输入86399以内的\n");
+	}
+}
